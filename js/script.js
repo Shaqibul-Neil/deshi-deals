4e52f58cb6be81c5179af9d3a7cddac8c404b1ea
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-08-26 05:20:37 +0600

    cart functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 "use strict";
+
+const btnsCart = document.querySelectorAll(".cart-btn");
+const inputCoupon = document.querySelector("#input-coupon");
+const btnApply = document.querySelector("#btn-apply");
+const card = document.querySelectorAll(".card");
+
+console.log(btnsCart, inputCoupon, btnApply, card);
+
+btnsCart.forEach((btnCart) => {
+  let hoverTimeout;
+  //mouseover effect
+  btnCart.addEventListener("mouseover", function () {
+    hoverTimeout = setTimeout(() => {
+      this.querySelector("span").classList.replace("opacity-100", "opacity-0");
+      //   this.querySelector("span").classList.add("opacity-0");
+      this.querySelector("i").classList.replace("opacity-0", "opacity-100");
+      //   this.querySelector("i").classList.remove("opacity-0");
+      //   this.querySelector("i").classList.add("opacity-100");
+    }, 100);
+  });
+  //mouseout effect
+  btnCart.addEventListener("mouseout", function () {
+    clearTimeout(hoverTimeout);
+    this.querySelector("span").classList.replace("opacity-0", "opacity-100");
+    // this.querySelector("span").classList.remove("opacity-0");
+    // this.querySelector("span").classList.add("opacity-100");
+    this.querySelector("i").classList.replace("opacity-100", "opacity-0");
+    // this.querySelector("i").classList.remove("opacity-100");
+    // this.querySelector("i").classList.add("opacity-0");
+  });
+  //click effect
+  btnCart.addEventListener("click", function (e) {
+    const priceCard =
+      this.closest(".card").querySelector(".product-price").textContent;
+    console.log(priceCard);
+  });
+});
