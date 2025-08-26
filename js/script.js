18e32963e054c5a625894e36a4a8978708092ebc
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-08-26 15:30:22 +0600

    cart container

--- js/script.js
+++ js/script.js
@@ -4,8 +4,12 @@ const btnsCart = document.querySelectorAll('.cart-btn');
 const inputCoupon = document.querySelector('#input-coupon');
 const btnApply = document.querySelector('#btn-apply');
 const card = document.querySelectorAll('.card');
+const totalPrice = document.querySelector('.total-price');
+const cartContainer = document.querySelector('#cart-container');
 
-console.log(btnsCart, inputCoupon, btnApply, card);
+// console.log(btnsCart, inputCoupon, btnApply, card);
+
+const timeout = function () {};
 
 //cart button functionality
 btnsCart.forEach(btnCart => {
@@ -31,9 +35,38 @@ btnsCart.forEach(btnCart => {
     // this.querySelector("i").classList.add("opacity-0");
   });
   //click effect
-  btnCart.addEventListener('click', function (e) {
+  btnCart.addEventListener('click', function () {
     const priceCard =
-      this.closest('.card').querySelector('.product-price').textContent;
+      +this.closest('.card').querySelector('.product-price').textContent;
     console.log(priceCard);
+    //calculating price
+    const existingTotalCartPrice = +totalPrice.textContent;
+    totalPrice.textContent = (existingTotalCartPrice + priceCard).toFixed(2);
+
+    //product data fetching
+    const currentCard = this.closest('.card');
+    const currentTitle = currentCard
+      .querySelector('.card-title')
+      .innerText.trim();
+    const currentPrice = currentCard
+      .querySelector('.product-price')
+      .innerText.trim();
+    const currentImgSrc = currentCard.querySelector('figure img').src;
+    //creating cart element
+    const HTML = ` 
+                <div
+                  class="card card-side shadow-sm bg-[#33303025] items-center h-[134px] mt-5"
+                >
+                  <figure class="h-[125px] w-[120px] p-4">
+                    <img src="${currentImgSrc}" alt="" class="w-full" />
+                  </figure>
+                  <div class="card-body">
+                    <h2 class="card-title font-semibold">${currentTitle}</h2>
+                    <p class="text-xl">
+                      <span class="product-price">${currentPrice}</span>TK
+                    </p>
+                  </div>
+                </div>`;
+    cartContainer.insertAdjacentHTML('afterbegin', HTML);
   });
 });
