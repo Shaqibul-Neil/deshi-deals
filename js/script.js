282b726d9fa2090c8433a231b579df556812d462
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-08-29 23:47:45 +0600

    promo btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 'use strict';
+const btnPromo = document.querySelector('.btn-promo');
 const productsContainer = document.querySelector('.products-container');
 const inputCoupon = document.querySelector('#input-coupon');
 const btnApply = document.querySelector('#btn-apply');
 const totalPrice = document.querySelector('.total-price');
 const cartContainer = document.querySelector('#cart-container');
 const totalQuantity = document.querySelector('.total-quantity');
+const totalDiscount = document.querySelector('.discount');
+const totalAfterDiscount = document.querySelector('.total-after-discount');
 
 // console.log(btnsCart, inputCoupon, btnApply, card);
 let count = 0;
@@ -102,5 +105,49 @@ cartContainer.addEventListener('click', function (e) {
 
     //removing cart item
     currentCard.remove();
+
+    // Check if discount should be reset
+    if (updatedPrice < 200 && btnApply.disabled) {
+      totalDiscount.innerText = 0;
+      totalAfterDiscount.innerText = updatedPrice.toFixed(2);
+      btnApply.disabled = false;
+      btnApply.innerText = 'Apply';
+    }
   } else return;
+});
+
+//promo code btn functionality. clicking on promo code btn will add code in the form
+btnPromo.addEventListener('click', function () {
+  inputCoupon.value = this.innerText;
+});
+
+//apply btn functionality
+btnApply.addEventListener('click', function (e) {
+  const inputValue = inputCoupon.value;
+  const promoCode = btnPromo.innerText;
+  const priceBeforeDiscount = +totalPrice.textContent;
+
+  //Already applied
+  if (btnApply.disabled) return;
+
+  //Wrong code
+  if (inputValue !== promoCode || !inputValue) {
+    alert(
+      `Wrong Code. You can click on the Promo Code Button for auto fill up`
+    );
+    return;
+  }
+
+  //Discount Applicable
+  if (inputValue === promoCode) {
+    if (priceBeforeDiscount >= 200) {
+      const discounted = +(priceBeforeDiscount * 0.2).toFixed(2);
+      totalDiscount.innerText = discounted;
+      totalAfterDiscount.innerText = priceBeforeDiscount - discounted;
+
+      //disable the apply btn after applying once
+      btnApply.disabled = true;
+      btnApply.innerText = 'Applied';
+    } else alert(`Please purchase 200 or more to get discount`);
+  }
 });
