23101c9e04c8269d38fb991de880b47fcffb852c
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-08-29 23:51:44 +0600

    add

--- js/script.js
+++ js/script.js
@@ -114,7 +114,7 @@ cartContainer.addEventListener('click', function (e) {
       btnApply.innerText = 'Apply';
     }
     if (btnApply.disabled) {
-      // Maintain discount if
+      // Maintain discount if total
     }
   } else return;
 });
